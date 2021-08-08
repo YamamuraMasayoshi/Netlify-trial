eaeeaaff8a19801e6aeeb8fc0552debdab8bca94
Yamamura Masayoshi <carotene_yama@yahoo.co.jp>
2021-08-08 11:23:07 +0900

    make it better

---
+++ app.py
@@ -0,0 +1,16 @@
+from git import Repo
+
+PATH_OF_GIT_REPO = r'C:\Users\carot\Desktop\Netlify-trial'  # make sure .git folder is properly configured
+COMMIT_MESSAGE = 'comment from python script'
+
+def git_Push():
+    try:
+        repo = Repo(PATH_OF_GIT_REPO)
+        repo.git.add(update=True)
+        repo.index.commit(COMMIT_MESSAGE)
+        Origin = repo.remote(name='Origin')
+        Origin.Push()
+    except:
+        print('Some error occured while pushing the code')    
+
+git_Push()
