0db3f7e28257b9b60d6afeea8962b2d791b05537
Yamamura Masayoshi <carotene_yama@yahoo.co.jp>
2021-09-12 14:50:49 +0900

    "Commit Log"

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import os
 import git
 
 #Pushしたいリポジトリに移動
-os.chdir(r'C:\Users\carot\Desktop\Netlify-trial')
+os.chdir(r'C:\Users\carot\OneDrive\デスクトップ\Netlify-trial')
 repo = git.Repo()
 
-#最新を取り込むため一旦Pull
+#最新を取り込むため一旦PullC:\Users\carot\OneDrive\デスクトップ\Netlify-trial
 o = repo.remotes.origin
 o.pull()
 
